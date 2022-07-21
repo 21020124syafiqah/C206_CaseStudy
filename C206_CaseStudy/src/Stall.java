31d4d074607db347b2a3c5e6a3ba5537990750cc
Nur Syafiqah <Nur Syafiqah@10.174.124.10>
2022-07-21 14:09:32 +0800

    Stall

--- C206_CaseStudy/src/Stall.java
+++ C206_CaseStudy/src/Stall.java
@@ -1,8 +1,16 @@
 import java.util.Date;
 
 public class Stall {
+	private String name;
 	private Date dateOfOperation;
 	
+	public Stall(String name, Date dateOfOperation) {
+		this.name = name;
+		this.dateOfOperation = dateOfOperation;
+	}
+	public String getName() {
+		return name;
+	}
 	public Date getDate() {
 		return dateOfOperation;
 	}
