5fd04044b09448e09863ccfdf8361577424909b8
Nur Syafiqah <Nur Syafiqah@10.174.124.10>
2022-07-22 11:47:29 +0800

    basics

--- C206_CaseStudy/src/C206_CaseStudy.java
+++ C206_CaseStudy/src/C206_CaseStudy.java
@@ -6,7 +6,30 @@ public class C206_CaseStudy {
 		// TODO Auto-generated method stub
 		ArrayList<Stall> stallList = new ArrayList<Stall>();
 		
-
+		stallList.add(new Stall("Western cuisine","17-9-2022"));
+		stallList.add(new Stall("Japanese cuisine","26-8-2022"));
+		stallList.add(new Stall("Chinese cuisine","9-8-2022"));
+		
+		
+		int option = 0;
+		
+		while (option != 4) {
+			
+		}
+	}
+	
+	public static void menu() {
+		C206_CaseStudy.setHeader("Canteen Automation System");
+		System.out.println("1.Add stall");
+		System.out.println("2.View stall");
+		System.out.println("3.Delete stall");
+		System.out.println("");
+	}
+	
+	public static void setHeader(String header) {
+		Helper.line(80, "-");
+		System.out.println(header);
+		Helper.line(80, "-");
 	}
 
 }
