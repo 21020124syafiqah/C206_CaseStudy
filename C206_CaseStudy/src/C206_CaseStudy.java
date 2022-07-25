587c4bd3dd137c85691e5a79a5b2c079b823c96a
Nur Syafiqah <Nur Syafiqah@10.174.124.10>
2022-07-25 14:39:29 +0800

    delete stalls

--- C206_CaseStudy/src/C206_CaseStudy.java
+++ C206_CaseStudy/src/C206_CaseStudy.java
@@ -6,9 +6,9 @@ public class C206_CaseStudy {
 		// TODO Auto-generated method stub
 		ArrayList<Stall> stallList = new ArrayList<Stall>();
 		
-		stallList.add(new Stall("Western cuisine","17-09-2022"));
-		stallList.add(new Stall("Japanese cuisine","26-08-2022"));
-		stallList.add(new Stall("Chinese cuisine","09-08-2022"));
+		stallList.add(new Stall("Western cuisine","S1", "17-09-2022"));
+		stallList.add(new Stall("Japanese cuisine","S2", "26-08-2022"));
+		stallList.add(new Stall("Chinese cuisine","S3", "09-08-2022"));
 		
 		
 		int option = 0;
@@ -22,7 +22,7 @@ public class C206_CaseStudy {
 			}else if(option == 2) {
 				viewAllStalls(stallList);
 			}else if(option == 3) {
-				
+				deleteStalls(stallList);
 			}else if(option == 4) {
 				System.out.println("Goodbye!");
 			}else {
@@ -48,9 +48,11 @@ public class C206_CaseStudy {
 	
 	public static Stall inputStall() {
 		String name = Helper.readString("Enter stall name > ");
+		String id = Helper.readString("Enter stall ID > ");
 		String date = Helper.readString("Enter date (dd-mm-yyyy) > ");
 		
-		Stall stall = new Stall(name, date);
+		
+		Stall stall = new Stall(name, id, date);
 		return stall;
 	}
 	
@@ -63,7 +65,7 @@ public class C206_CaseStudy {
 		String output = "";
 		
 		for (int i = 0; i < stallList.size(); i++) {
-			output += String.format("%-60s \n", stallList.get(i).toString());
+			output += String.format("%-90s \n", stallList.get(i).toString());
 		}
 		
 		return output;
@@ -72,9 +74,25 @@ public class C206_CaseStudy {
 	public static void viewAllStalls(ArrayList<Stall> stallList) {
 		C206_CaseStudy.setHeader("STALL LIST");
 		
-		String output = String.format("%-20s %-70s\n", "NAME", "DATE OF OPERATION");
+		String output = String.format("%-20s %-10s %-10s\n", "NAME", "ID", "DATE OF OPERATION");
 	    output += retrieveAllStalls(stallList);
 	    System.out.println(output);
 	}
+	
+	public static void deleteStalls(ArrayList<Stall> stallList) {
+		String deleteID = Helper.readString("Enter stall ID to delete > ");
+		for (int i = 0; i < stallList.size(); i++) {
+			if(stallList.get(i).getId().equalsIgnoreCase(deleteID)) {
+				String sure = Helper.readString("Are you sure you want to delete stall " + stallList.get(i).getId() + "? (y/n) > ");
+				if(sure.equals("y")) {
+					stallList.remove(i);
+					System.out.println("Stall removed");
+				}else if(sure.equals("n")){
+					
+				}else {
+					System.out.println("Wrong input");
+				}
+		}
+	}
 
-}
+}}
--- C206_CaseStudy/src/Stall.java
+++ C206_CaseStudy/src/Stall.java
@@ -3,14 +3,16 @@ import java.util.Date;
 public class Stall {
 	private String name;
 	private String dateOfOperation;
+	private String id;
 	
-	public Stall(String name, String dateOfOperation) {
+	public Stall(String name, String id, String dateOfOperation) {
 		this.name = name;
 		this.dateOfOperation = dateOfOperation;
+		this.id = id;
 	}
 	
 	public String toString() {
-		String stallInfo = String.format("%-20s %-60s", name, dateOfOperation);
+		String stallInfo = String.format("%-20s %-10s %-10s", name, id, dateOfOperation);
 		
 		return stallInfo;
 	}
@@ -21,6 +23,10 @@ public class Stall {
 	
 	public String getDate() {
 		return dateOfOperation;
+	}
+	
+	public String getId() {
+		return id;
 	}
 
 }
