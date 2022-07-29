941c61c75bc3f440d619dd1e7630f92ce4c4b674
Nur Syafiqah <Nur Syafiqah@10.174.124.10>
2022-07-29 10:31:33 +0800

    junit basics

--- C206_CaseStudy/test/C206_CaseStudyTest.java
+++ C206_CaseStudy/test/C206_CaseStudyTest.java
@@ -1,13 +1,25 @@
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
+
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
 public class C206_CaseStudyTest {
+	private Stall S1;
+	private Stall S2;
+	private Stall S3;
+	
+	private ArrayList<Stall> stallList;
 
 	@Before
 	public void setUp() throws Exception {
+		S1 = new Stall("Western cuisine","S1", "17-09-2022");
+		S2 = new Stall("Japanese cuisine","S2", "26-08-2022");
+		S3 = new Stall("Chinese cuisine","S3", "09-08-2022");
+		
+		stallList = new ArrayList<Stall>();
 	}
 
 	@After
@@ -18,6 +30,18 @@ public class C206_CaseStudyTest {
 	public void c206_test() {
 		//fail("Not yet implemented"); 
 		assertTrue("C206_CaseStudy_SampleTest ",true);
+	}
+	
+	public void testAddStall() {
+		
+	}
+	
+	public void testViewStall() {
+		
+	}
+	
+	public void testDeleteStall() {
+		
 	}
 
 }
