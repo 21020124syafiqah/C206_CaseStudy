1481d791cbc995d8add000bb3679b7651f5b274f
21006685 <21006685@10.174.124.4>
2022-08-05 11:01:52 +0800

    New

--- C206_CaseStudy/src/C206_CaseStudy.java
+++ C206_CaseStudy/src/C206_CaseStudy.java
@@ -80,7 +80,7 @@ public class C206_CaseStudy {
 					System.out.println("Invalid option entered!");
 					}
 			}else if(option == 3) {
-				//Rishwin
+				//rishwin
 				
 				C206_CaseStudy.menu();
 				int COption = Helper.readInt("Enter an option > ");
